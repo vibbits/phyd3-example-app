4f1a596b3e8023815c5c0d7bf522fea25a782c4c
James Collier <james.collier@vib.be>
2023-01-09 15:49:15 +0100

    Export the drawTree function

--- src/app.js
+++ src/app.js
@@ -1,23 +1,10 @@
 import { makeCompatTable, phyloxml } from "@vibbioinfocore/phyd3-parser-compat";
 import { build } from "@vibbioinfocore/phyd3";
 
-const drawTree = (serialised) => {
+export const drawTree = (serialised) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(serialised, "text/xml");
   const svg = build(makeCompatTable(phyloxml.parse(doc)));
 
-  document.querySelector("#phyd3-root").appendChild(svg.node());
+  return svg.node();
 };
-
-window.addEventListener("load", () => {
-  document.querySelector("#phyd3-display").addEventListener("click", () => {
-    drawTree(document.querySelector("#phyd3-input").value);
-  });
-
-  document.querySelector("#phyd3-clear").addEventListener("click", () => {
-    const root = document.querySelector("#phyd3-root");
-    while (root.firstChild) {
-      root.removeChild(root.firstChild);
-    }
-  });
-});
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { drawTree } from "./app.js";
+
+window.addEventListener("load", () => {
+  document.querySelector("#phyd3-display").addEventListener("click", () => {
+    document
+      .querySelector("#phyd3-root")
+      .appendChild(drawTree(document.querySelector("#phyd3-input").value));
+  });
+
+  document.querySelector("#phyd3-clear").addEventListener("click", () => {
+    const root = document.querySelector("#phyd3-root");
+    while (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+  });
+});
