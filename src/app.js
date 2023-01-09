1dd725e1b4e55cb6469b860c8a416af323b4f2d7
James Collier <james.collier@vib.be>
2023-01-09 16:08:32 +0100

    Build output should be esm

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
-import { makeCompatTable, phyloxml } from "@vibbioinfocore/phyd3-parser-compat";
-import { build } from "@vibbioinfocore/phyd3";
+import drawTree from "./index.js";
 
-export const drawTree = (serialised) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(serialised, "text/xml");
-  const svg = build(makeCompatTable(phyloxml.parse(doc)));
+window.addEventListener("load", () => {
+  document.querySelector("#phyd3-display").addEventListener("click", () => {
+    document
+      .querySelector("#phyd3-root")
+      .appendChild(drawTree(document.querySelector("#phyd3-input").value));
+  });
 
-  return svg.node();
-};
+  document.querySelector("#phyd3-clear").addEventListener("click", () => {
+    const root = document.querySelector("#phyd3-root");
+    while (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import { drawTree } from "./app.js";
+import { makeCompatTable, phyloxml } from "@vibbioinfocore/phyd3-parser-compat";
+import { build } from "@vibbioinfocore/phyd3";
 
-window.addEventListener("load", () => {
-  document.querySelector("#phyd3-display").addEventListener("click", () => {
-    document
-      .querySelector("#phyd3-root")
-      .appendChild(drawTree(document.querySelector("#phyd3-input").value));
-  });
+export const drawTree = (serialised) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(serialised, "text/xml");
+  const svg = build(makeCompatTable(phyloxml.parse(doc)));
 
-  document.querySelector("#phyd3-clear").addEventListener("click", () => {
-    const root = document.querySelector("#phyd3-root");
-    while (root.firstChild) {
-      root.removeChild(root.firstChild);
-    }
-  });
-});
+  return svg.node();
+};
